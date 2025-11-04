806c06e7c797a6b594acd8f43e60bc03db725f8c
iseoria <dongtato@naver.com>
2025-11-04 16:03:58 +0900

    chore: keep only README/.gitignore/.env.example

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./app/router";
-import "./shared/styles/index.css";
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><RouterProvider router={router} /></React.StrictMode>
-);
